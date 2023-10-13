1f64a9437192bcea5b65274ed8f7982cade28bc8
Abhijit Das <46588834+thehulkbro@users.noreply.github.com>
2023-10-13 10:50:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
-
+ 
+// Function to remove articles ('a', 'an', 'the') from a string
+function removeArticles(name) {
+  return name.replace(/^(a|an|the)\s+/i, '').trim();
+}
+ 
+// Array of band names
+let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+ 
+// Remove articles and sort the band names
+let sortedBandNames = bandNames.map(removeArticles).sort();
+ 
+// Create an HTML list
+const ul = document.createElement('ul');
+ul.id = 'band';
+ 
+sortedBandNames.forEach(name => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  ul.appendChild(li);
+});
+ 
+// Append the list to the document's body or another desired location
+document.body.appendChild(ul);
